8b0ab8bf082c9597291731ba822cb5553fe30495
julianopucmg <1435615@sga.pucminas.br>
2023-06-27 22:19:04 -0300

    Adicionando tabela transacao

--- myfinance-web-netcore/docs/myfinance.sql
+++ myfinance-web-netcore/docs/myfinance.sql
@@ -1,8 +1,9 @@
 /* Criar Banco de Dados */
 CREATE DATABASE myfinance
+GO
 
 USE myfinance
-
+GO
 
 /* Estrutura do Banco de dados */
 CREATE TABLE planoconta(
@@ -10,7 +11,9 @@ CREATE TABLE planoconta(
 	descricao VARCHAR(50) NOT NULL,
 	tipo CHAR(1),
 	PRIMARY KEY (id)
-);
+	
+)
+GO
 
 /* Inserir primeira carga de dados */
 INSERT INTO planoconta(descricao, tipo) VALUES('Combustível', 'D')
@@ -20,7 +23,27 @@ INSERT INTO planoconta(descricao, tipo) VALUES('Impostos', 'D')
 INSERT INTO planoconta(descricao, tipo) VALUES('Água', 'D')
 INSERT INTO planoconta(descricao, tipo) VALUES('Luz', 'D')
 INSERT INTO planoconta(descricao, tipo) VALUES('Internet', 'D')
+INSERT INTO planoconta(descricao, tipo) VALUES('Cartão de Crédito', 'D')
 
 
+/* Estrutura do Banco de dados */
+CREATE TABLE transacao(
+    id int IDENTITY(1,1) NOT NULL,
+    data datetime NOT NULL,
+    valor decimal(9,2) NOT NULL,
+    historico varchar(100) NULL,
+    planoconta_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (planoconta_id) REFERENCES planoconta(id)
+)
+GO
 
-
+/* Inserir primeira carga de dados */
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES(GETDATE(), 25, 'Café da Manhã', 3)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES(GETDATE(), 38, 'Gasolina Moto', 1)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES(GETDATE(), 100, 'Salario Empresa 1', 2)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES('20230610', 350, 'IPTU', 4)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES('20230608', 250, 'Copasa', 5)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES('20230608', 350, 'CEMIG', 6)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES('20230605', 125, 'Internet Casa', 7)
+INSERT INTO transacao(data, valor, historico, planoconta_id) VALUES('20230612', 600, 'Cartão Santander', 8)
